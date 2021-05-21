8f23fb2767958ec9ef1e78e30722019f77de7597
Angus Hollands <goosey15@gmail.com>
2021-05-21 13:52:05 +0100

    WIP: initial commit

---
+++ snek/__init__.py
@@ -0,0 +1,12 @@
+def print_snek():
+    print("""\
+    This is a *big* snek.
+    --..,_                     _,.--.
+       `'.'.                .'`__ o  `;__.
+          '.'.            .'.'`  '---'`  `
+            '.`'--....--'`.'
+              `'--....--'`
+""")
+
+def sizeof_plugin(sizeof):
+    print_snek()
